5d79ea1d2b24a347f8f9c68ae948e0dbc3c647e6
Christopher Watson <christopher_watson@icloud.com>
2018-07-24 15:54:56 -0400

    backend functions

---
+++ controllers/backupController.js
@@ -0,0 +1,37 @@
+const Backup = require('../models/Backup');
+
+// controller methods
+module.exports = {
+  findAll: function(req, res) {
+    Backup
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    Backup
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    Backup
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    Backup
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    Backup
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
