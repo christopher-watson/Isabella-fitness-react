cbd53b58dd4223aa1f638f375c3cd2fb6b1f261a
Christopher Watson <christopher_watson@icloud.com>
2018-07-24 22:09:50 -0400

    backend

--- routes/api/backupRoutes.js
+++ routes/api/backupRoutes.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 const backupController = require('../../controllers/backupController');
 
-// ../api/clients
+// ../api/backup
 router
   .route("/")
   .get(backupController.findAll)
   .post(backupController.create);
 
-// ../api/clients/:id
-router
-  .route("/:id")
-  .get(backupController.findById)
-  .put(backupController.update)
-  .delete(backupController.remove);
+// ../api/backup/:id
+// router
+//   .route("/:id")
+//   .get(backupController.findById)
+//   .put(backupController.update)
+//   .delete(backupController.remove);
 
 module.exports = router;
