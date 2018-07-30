8629b3be3faa4bca52087fc25041515896900255
Christopher Watson <christopher_watson@icloud.com>
2018-07-30 15:27:28 -0400

    client delete response done

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import API from "../../utils/API";
+
+export default class Form extends React.Component {
+  state = {
+
+  };
+
+  render( ) {
+
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
